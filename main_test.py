f2b9a874e060fa738259e2a23ad1d0ab27e1b1a8
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-12 12:54:13 -0500

    added unit tests for each function in main.py

--- main_test.py
+++ main_test.py
@@ -1,8 +1,40 @@
 import unittest
+import main
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_sum_to_6(self):
+        self.assertEqual(main.sum_to(6), 21)
+
+    def test_sum_to_10(self):
+        self.assertEqual(main.sum_to(10), 55)
+
+    def test_largest_1(self):
+        self.assertEqual(main.largest([1, 2, 3, 4, 0]), 4)
+
+    def test_largest_2(self):
+        self.assertEqual(main.largest([10, 4, 2, 231, 91, 54]), 231)
+
+    def test_occurrences_1(self):
+        self.assertEqual(main.occurrences('fleep floop', 'e'), 2)
+
+    def test_occurrences_2(self):
+        self.assertEqual(main.occurrences('fleep floop', 'p'), 2)
+
+    def test_occurrences_3(self):
+        self.assertEqual(main.occurrences('fleep floop', 'ee'), 1)
+
+    def test_occurrences_4(self):
+        self.assertEqual(main.occurrences('fleep floop', 'fe'), 0)
+
+    def test_product_1(self):
+        self.assertEqual(main.product(-1, 4), -4)
+
+    def test_product_2(self):
+        self.assertEqual(main.product(2, 5, 5), 50)
+
+    def test_product_3(self):
+        self.assertEqual(main.product(4, 0.5, 5), 10)
 
 if __name__ == '__main__':
     unittest.main()
