b9747dd7ccdbf6ff3267b0a6efce3e28193d6f5e
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-12 12:55:51 -0500

    Removed "for example" comments since i have tests

--- main.py
+++ main.py
@@ -2,11 +2,7 @@
 Write a function named sum_to
 that accepts a single integer, n,
 and returns the sum of the integers
-from 1 to n. For example:
-
-    sum_to(6)  # returns 21
-    sum_to(10) # returns 55
-
+from 1 to n.
 """
 
 
@@ -18,12 +14,6 @@ Write a function named largest
 that takes a list of numbers as an
 argument and returns the largest
 number in that list.
-
-For example:
-
-    largest([1, 2, 3, 4, 0])  # returns 4
-    largest([10, 4, 2, 231, 91, 54])  # returns 231
-
 """
 
 
@@ -37,14 +27,6 @@ that takes two string arguments as
 input and counts the number of
 occurrences of the second string
 inside the first string.
-
-For example:
-
-    occurrences('fleep floop', 'e')   # returns 2
-    occurrences('fleep floop', 'p')   # returns 2
-    occurrences('fleep floop', 'ee')  # returns 1
-    occupancies('fleep floop', 'fe')  # returns 0
-
 """
 
 
@@ -58,13 +40,6 @@ takes an arbitrary number of numbers,
 multiplies them all together, and
 returns the product.
 (HINT: Review your notes on *args).
-
-For example:
-
-    product(-1, 4) # returns -4
-    product(2, 5, 5) # returns 50
-    product(4, 0.5, 5) # returns 10.0
-
 """
 
 
